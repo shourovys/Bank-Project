125feca408522c1e913ed240b7320a6408ffdef5
shourov <shourovys@gmail.com>
2020-06-12 23:23:24 +0600

    all html and css added

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById("log-in-btn").addEventListener("click", function () {
+
+    document.getElementById("log-in").style.display = "none";
+    document.getElementById("bank-acc").style.display = "block";
+
+})
+
+
+
+
+// ---------------------------------account management
+// deposit management
+
+// const current-deposit = document.getElementById("current-deposit").innerHTML
